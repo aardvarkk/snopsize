29e1f741324db518a5a7599481c9591170d76d64
Ian Clarkson <clarksonian@gmail.com>
2012-07-17 19:31:49 -0400

    Created a project for snopsize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snopsize::Application.config.secret_token = '30f3096e88ed07cf55e5dbe29408734dfae74f02c41f205c78d162d40e007bcc3922170a23ca5f1409fe1ad7c52739104146509c4237fe3f0bdf440c4e0147b9'
