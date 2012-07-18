454314fd2abd6a818256d4b43e607bad23750c15
Dinko Harbinja <dharbinja@yahoo.ca>
2012-07-17 22:15:47 -0400

    Added very basic devise functionality using Railscast found online. Added "Home" controller that gives us a "Home#Index" route

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ end
 
 gem 'jquery-rails'
 
+#To use devise authentication
+gem 'devise'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Snopsize::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Set default url options
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
