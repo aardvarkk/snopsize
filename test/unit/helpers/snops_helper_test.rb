8e44be98ec94b4c6c0992bbeef349c1a7005cdfa
Ian Clarkson <clarksonian@gmail.com>
2012-07-18 16:00:42 -0400

    Adding basic Snop scaffolding.

---
+++ test/unit/helpers/snops_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SnopsHelperTest < ActionView::TestCase
+end
---
+++ test/unit/snop_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SnopTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
