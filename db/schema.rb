257c89e7e311ddb037b16fd10a5b3bb00aaa4168
Dinko Harbinja <dharbinja@yahoo.ca>
2012-07-18 22:06:31 -0400

    Added login based on "username" instead of "email"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120718195540) do
+ActiveRecord::Schema.define(:version => 20120719013018) do
 
   create_table "snops", :force => true do |t|
     t.integer  "user_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20120718195540) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
