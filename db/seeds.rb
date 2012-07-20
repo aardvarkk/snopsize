3f5f1513929a888e358babd29cc99739e4b67b02
Ian Clarkson <clarksonian@gmail.com>
2012-07-20 16:25:32 -0400

    Automatically reindexing upon snop creation, so new snops are searchable without a manual reindex.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create(:username => 'fred', :email => 'fred@fredtastic.com', :password => '
 Snop.create(
 	:user_id => 1, 
 	:domain => 'http://www.theglobeandmail.com',
-	:resource => '/news/world/12-dead-after-masked-gunman-opens-fire-at-batman-premiere/article4429306/',
+	:resource => '/news/world/12-dead-after-masked-gunman-opens-fire-at-batman-premiere/article4429306',
 	:title => "71 people shot, 12 fatally at Colorado theatre's Batman premiere",
 	:point1 => 'Point 1',
 	:point2 => 'Point 2',
@@ -24,10 +24,26 @@ Snop.create(
 Snop.create(
 	:user_id => 2, 
 	:domain => 'http://www.theglobeandmail.com',
-	:resource => '/news/politics/ottawa-stalls-khadr-repatriation-with-new-roadblock/article4430435/',
+	:resource => '/news/politics/ottawa-stalls-khadr-repatriation-with-new-roadblock/article4430435',
 	:title => 'Ottawa stalls Khadr repatriation with new roadblock',
 	:point1 => 'Point 1',
 	:point2 => 'Point 2',
 	:point3 => 'Point 3',
 	:summary => 'My summary'
 	)
+
+Snop.create(
+	:user_id => 3, 
+	:domain => 'http://www.economist.com',
+	:resource => '/node/21559367',
+	:title => 'How long can the regime last?',
+	:point1 => 'Point 1',
+	:point2 => 'Point 2',
+	:point3 => 'Point 3',
+	:summary => 'My summary'
+	)
+
+# Required to index the snops we've created
+# http://stackoverflow.com/questions/8088686/sunspot-not-indexing-objects-created-in-rake-tasks
+Sunspot.index
+Sunspot.commit
