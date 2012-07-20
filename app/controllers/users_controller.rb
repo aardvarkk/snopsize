6d476a9d7cf53991558b8f0cbfd7e999ca6d0d38
Dinko Harbinja <dinko@taulabs.ca>
2012-07-20 16:25:50 -0400

    Adding "Created Snops" section to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
   	@user = User.find(params[:id])
+  	
+  	# Get a list of all the users created snops
+  	# (i.e. snops created by the user)
+  	@usersnops = Snop.where(["user_id = ?", @user.id]).all
+  	
+  	# Get a list of the users favourite snops
   	@fave_snops = FaveSnops.where(["user_id = ?", @user.id]).all
   	@snops = Array.new
   	@fave_snops.each do |fave_snop|
