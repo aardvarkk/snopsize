2cb6507d3627e6c4cea9d887dd3b9cf5d8f344cb
Ian Clarkson <clarksonian@gmail.com>
2012-08-08 11:27:20 -0400

    Removing URI validator -- only using Public Suffix List and basic heuristic parsing from Addressable::URI now.

--- app/validators/uri_validator.rb
+++ app/validators/uri_validator.rb
@@ -1,7 +1,28 @@
-class UriValidator < ActiveModel::EachValidator
-  include UriHelper
+require 'addressable/uri'
+require 'public_suffix'
 
+class UriValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, "Invalid URL") unless get_response(Addressable::URI.parse(value)).class <= Net::HTTPSuccess
+
+    # If it's blank, it's fine -- we don't need a URI
+    return if value.blank?
+
+    # Try to parse it as a URI in a forgiving manner
+    begin
+      uri = Addressable::URI.heuristic_parse(value);
+    rescue
+      record.errors.add(attribute, 'Invalid URL')
+    end
+
+    # Check that it's HTTP
+    if uri.scheme != 'http'
+      record.errors.add(attribute, 'URL must be HTTP')      
+    end
+
+    # Check with public suffix list
+    if !PublicSuffix.valid?(uri.host)
+      record.errors.add(attribute, 'Invalid public suffix')
+    end
+    
   end
 end
