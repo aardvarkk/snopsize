24c7b1e070a83c97a70f06e2c3b201ff66a4c68c
Dinko Harbinja <dinko@taulabs.ca>
2012-08-08 12:49:00 -0400

    Adding first integration test!

---
+++ test/integration/create_new_snop_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+# Story: A user comes to snopsize.com, logs in, and creates
+# a new snop.
+class CreateNewSnopTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
