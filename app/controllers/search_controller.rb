cd9aa97d43caa8b61fb587e52f6819fe661106f9
Ian Clarkson <clarksonian@gmail.com>
2012-08-08 16:41:51 -0400

    Adding text-based user search. Default is keyword-based, but you can select via a select box.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,31 @@ class SearchController < ApplicationController
   
   # GET /search
   def search
-  	# assume results are nil to start with
+  	
+    # assume results are nil to start with
   	@results = nil
 
   	# we're done if they didn't pass a search query
-  	return unless (params[:q])
+  	return unless params[:q]
 
-  	# perform the search using the full text
-  	@results = Snop.search do
-  		fulltext params[:q]
-  	end.results
+    # by default, perform a keyword search
+    case params[:type]
+    when 'url'
+    when 'user'
+
+      # perform user search using the full text
+      @results = User.search do
+        fulltext params[:q]
+      end.results
+
+    else 
+
+      # perform keyword search using the full text
+      @results = Snop.search do
+        fulltext params[:q]
+      end.results
+
+    end
+
   end
 end
