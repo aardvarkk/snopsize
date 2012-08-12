dd46a6f801f9a7d80fd67eadf4bd1ec17cad0fc7
Dinko Harbinja <dinko@taulabs.ca>
2012-08-12 00:21:00 -0400

    closes #35. Since domains are not case sensitive, I'm making it the same in our Domain model. The domain model now downcases any URI it receives. I've also added a corresponding test.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -3,9 +3,20 @@ class Domain < ActiveRecord::Base
 	has_many :resources
 	has_many :snops
 
+  # Callbacks
+  before_validation :downcase_uri
+
 	# Validations
 	# must have a uri (can't be null), and must be unique
 	validates :uri, :presence => true, :uniqueness => true
 
   attr_accessible :uri
+
+  # We'll only deal with lowercase Domain URIs since 
+  # Domains are not case sensitive.
+  def downcase_uri 
+    if self.uri
+      self.uri = uri.downcase
+    end
+  end
 end
--- test/unit/domain_test.rb
+++ test/unit/domain_test.rb
@@ -13,11 +13,32 @@ class DomainTest < ActiveSupport::TestCase
     assert domain.save, "Can't save domain!"
   end
 
+  # Shouldn't be allowed to add a domain with the same URI
   test "domain uniqueness" do
-    # Shouldn't be allowed to add a domain with the same URI
     domain = Domain.new(
       :uri => domains(:one).uri
       )
     assert !domain.save, "Saved a Domain with same URI!"
+  end
+
+  # Domains should not be case sensitive! That is 
+  # www.globeandmail.com is the same as WWW.GLOBEANDMAIL.COM
+  test "domain case insensitivity" do
+    domain = Domain.new(
+      :uri => "www.testdomain.com"
+      )
+    assert domain.save, "Unable to save valid domain!"
+
+    # Now try to create the same one but with all uppercase
+    domain = Domain.new(
+      :uri => "WWW.TESTDOMAIN.COM"
+      )
+    assert !domain.save, "Saved uppercase version of existing domain!"
+
+    # Now try some more crazy stuff with the domain... it still shouldn't pass
+    domain = Domain.new(
+      :uri => "www.TESTDOMAIN.coM"
+      )
+    assert !domain.save, "Saved crazy case sensitive domain!"
   end
 end
