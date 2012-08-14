ba073300bedff027f798c4d8b77dd4c2fa6c54af
Ian Clarkson <clarksonian@gmail.com>
2012-08-14 15:37:25 -0400

    No longer showing deleted snops on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # Get a list of the latest snops
-    @latest_snops = Snop.order("created_at DESC").limit(5)
+    @latest_snops = Snop.where(:deleted => false).order("created_at DESC").limit(5)
 
     # Get a list of the most popular snops
     @popular_snops = Snop.includes(:fave_snops).order("count(fave_snops.snop_id) DESC").group("fave_snops.snop_id").having("fave_snops.snop_id > ?", 0).limit(5)
