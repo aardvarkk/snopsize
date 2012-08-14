1c373ec237d2a1c28304493dd270be13da732007
Ian Clarkson <clarksonian@gmail.com>
2012-08-14 17:21:14 -0400

    No longer showing deleted snops in the list of popular snops.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
     @latest_snops = Snop.where(:deleted => false).order("created_at DESC").limit(5)
 
     # Get a list of the most popular snops
-    @popular_snops = Snop.includes(:fave_snops).order("count(fave_snops.snop_id) DESC").group("fave_snops.snop_id").having("fave_snops.snop_id > ?", 0).limit(5)
+    @popular_snops = Snop.where(:deleted => false).includes(:fave_snops).order("count(fave_snops.snop_id) DESC").group("fave_snops.snop_id").having("fave_snops.snop_id > ?", 0).limit(5)
   end
 end
