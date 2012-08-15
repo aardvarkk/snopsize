cfc52437be862e13f1b49fb5646dd1aee95df1e3
Dinko Harbinja <dinko@taulabs.ca>
2012-08-14 22:10:35 -0400

    Added some more tests to the search controller. Closes #39.

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -3,9 +3,59 @@ require 'test_helper'
 class SearchControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test "should get search" do
-    # Try to get the search page
+  test "should get search with no type" do
+    # Try to get the search page with no type passed
     get :search
+
+    # Should be a successful query
+    assert_response :success
+
+    # Make sure the results are set to nil
+    assert_nil assigns(:results)
+  end
+
+  test "should get keyword search" do
+    # Try to get the search page with keyword search
+    # specified this time
+    get :search, type: "keyword"
+
+    # Should be a successful query
+    assert_response :success
+
+    # Make sure the results are nil
+    assert_nil assigns(:results)
+
+    # Now lets try it with some data
+    get :search, type: "keyword", q: "point"
+
+    # Shoud be a successful query
+    assert_response :success
+
+    # Make sure results are not nil
+    assert_not_nil assigns(:results)
+  end
+
+  test "should get url search" do
+    # Try to get the search page
+    get :search, type: "url"
+
+    # Should be a successful query
+    assert_response :success
+
+    # Make sure the results are set
+    assert_nil assigns(:results)
+
+    # Now lets try with some data
+    get :search, type: "url", q: "www.test.com/query"
+
+    # Should be a successful query
+    assert_response :success
+
+    # Make sure the results are set
+    assert_not_nil assigns(:results)
+
+    # Now lets try with an invalid uri
+    get :search, type: "url", q: "somegibberish"
 
     # Should be a successful query
     assert_response :success
@@ -14,9 +64,18 @@ class SearchControllerTest < ActionController::TestCase
     assert_nil assigns(:results)
   end
 
-  test "should get search with results" do
+  test "should get user search" do
     # Try to get the search page
-    get :search, q: "test"
+    get :search, type: "user"
+
+    # Should be a successful query
+    assert_response :success
+
+    # Make sure the results are set
+    assert_nil assigns(:results)
+
+    # Now lets try with some data
+    get :search, type: "user", q: "auser"
 
     # Should be a successful query
     assert_response :success
