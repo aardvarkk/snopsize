64e676be40d60853e49c2d2156e003b2a92989fd
Ian Clarkson <clarksonian@gmail.com>
2012-08-28 15:36:12 -0400

    Showing ads to the adviewer user in the home view of popular snops.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,23 @@ class HomeController < ApplicationController
     @snops = Snop.where(deleted: false, is_ad: false).order("popularity DESC").limit(5).to_a
     @snop = Snop.find(params[:snop]) if params[:snop]
 
+    # The list view shouldn't show the ads
+    snops_no_ads = @snops.dup;
+
+    # If we're the adviewer, plop some ads into the snops
+    # They'll get shown in the browse view
+    if auth_signed_in? && current_auth.username == 'adviewer'
+      # Get all of the ad snops... limiting to some number
+      # This doesn't have to work like this, but it's easy to start with
+      @ads = Snop.where(deleted: false, is_ad: true).to_a
+
+      # Insert the ads
+      (1..@snops.length).step(5).each { |i| @snops.insert(i, @ads[rand(@ads.length)]) }
+    end
+
     respond_to do |format|
       format.html
-      format.json { render json: HomeTable.new(view_context, @snops) }
+      format.json { render json: HomeTable.new(view_context, snops_no_ads) }
       format.js 
     end
 
