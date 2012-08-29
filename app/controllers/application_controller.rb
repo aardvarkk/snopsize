38c06b2bb152d278585fee60388d2a284632a456
Dinko Harbinja <dinko@taulabs.ca>
2012-08-28 21:30:54 -0400

    Fixes #70. Fixed the issue where the back/forward buttons were showing cached JS. This is because our AJAX GET responses were being cached. I disabled browser caching for AJAX requests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :remove_cache_for_ajax
+
+  def remove_cache_for_ajax
+    # Please see below link for why this has to be done.
+    # http://stackoverflow.com/questions/711418/how-to-prevent-browser-page-caching-in-rails
+
+    # If the request is an AJAX request, we will tell the browser not to cache, the results.
+    # This means that any AJAX GET responses will not be cached in the browse and then shown
+    # via the back and forward buttons.
+    if request.xhr?
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
+  end
 end
