f9ca416e098bddfc421ef27219507db83555b0be
Dinko Harbinja <dinko@taulabs.ca>
2012-09-20 21:16:15 -0400

    Fixes #95. Redirecting to the page we came from after sign-in. Had to add a few checks to make sure we didn't get into any redirect loops or that the session variable was null (in case of tests).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :remove_cache_for_ajax
+  after_filter :store_location
 
+  # Please see below link for why this has to be done.
+  # http://stackoverflow.com/questions/711418/how-to-prevent-browser-page-caching-in-rails
   def remove_cache_for_ajax
-    # Please see below link for why this has to be done.
-    # http://stackoverflow.com/questions/711418/how-to-prevent-browser-page-caching-in-rails
-
     # If the request is an AJAX request, we will tell the browser not to cache, the results.
     # This means that any AJAX GET responses will not be cached in the browse and then shown
     # via the back and forward buttons.
@@ -15,5 +15,27 @@ class ApplicationController < ActionController::Base
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
     end
+  end
+
+  # Stores unique previous URLs (only the last two, so we always know where we came from)
+  def store_location
+    session[:previous_urls] ||= []
+    # store unique urls only that are not AJAX (just page changes)
+    unless request.xhr?
+      session[:previous_urls].prepend request.fullpath if session[:previous_urls].first != request.fullpath
+      session[:previous_urls].pop if session[:previous_urls].count > 2
+    end
+  end
+
+  # Where to go after signing in. Well we just want to go to the last page they were on.
+  # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in
+  def after_sign_in_path_for(resource)
+    # For some tests sessions aren't set, so we'll make sure the session isn't nil before using.
+    # We also want to make sure we're not redirecting to the same URL, this could cause a loop.
+    if session[:previous_urls].nil? || session[:previous_urls].last == request.fullpath
+      return root_path
+    end
+
+    return session[:previous_urls].last 
   end
 end
