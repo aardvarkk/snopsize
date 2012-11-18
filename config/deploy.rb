8925d2b532fb59aac890180c1c073f1f6a4c6990
aardvarkk <clarksonian@gmail.com>
2012-11-18 01:44:03 -0500

    Need to check in Gemfile.lock to get all the right gems.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,21 @@
 require "bundler/capistrano" # runs bundler install on the server
 require "rvm/capistrano"
 
+before 'deploy', 'rvm:install_rvm'
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+before 'deploy', 'rvm:install_ruby'
+
 set :user, 				'sysadmin'
 set :application, 'snopsize'
 set :repository,  'git@github.com:aardvarkk/snopsize.git'
 set :deploy_to,   '/home/sysadmin/snopsize'
 set :port, 8285
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# Seems to fix error about 'no tty present and no askpass program specified'
-default_run_options[:pty] = true
 
 # Your HTTP server, Apache/etc
 role :app, 'snopsize.com'
 role :web, 'snopsize.com'
 role :db,  'snopsize.com', :primary => true
-
-# This lets us NOT have a Gemfile.lock
-# I think it fixes an error saying deployment can't find Gemfile.lock
-set :bundle_flags, "--quiet"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -29,7 +25,6 @@ set :bundle_flags, "--quiet"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
